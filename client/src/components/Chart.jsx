bde583ba076edd5eeaa105956a44e797110050fa
Keaton Tatooles <keatontatooles@Keatons-MacBook-Air.local>
2019-08-04 18:38:35 -0700

    configured initial server request

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import { Bar, Line, Pie } from 'react-chartjs-2';
+import axios from 'axios';
+
+//check out default properties 
 
 class Chart extends Component {
     constructor(props) {
@@ -29,19 +32,46 @@ class Chart extends Component {
                     ],
                     borderWidth: 1
                 }]
+            },
+
+            line: {
+                labels: [1,2,3,4,5],
+                datasets: [{
+                    data: [10,20,35, 50, 80],
+                    label: 'test1',
+                }]
             }
         }
+
+        this.getStockData = this.getStockData.bind(this);
     }
+
+    componentDidMount() {
+        this.getStockData('AAPL');
+    }
+
+    getStockData(ticker) {
+      axios.get(`/stocks/${ticker}`)
+       .then((data) => {
+           console.log(data)
+       })
+    }
+
     render() {
         return (
             <div className="chart">
-              <Bar
+              {/* <Bar
                 data={this.state.chartData}
                 options={{ maintainAspectRatio: false, title: {
                     display: true,
                     text: 'Test Graph',
                 } }}
-                />  
+                height={50}
+                />   */}
+              <Line 
+               data={this.state.line}
+               options={{ maintainAspectRatio: false }}
+               />
             </div>
         )
     }
--- server/index.js
+++ server/index.js
@@ -2,12 +2,39 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 2000;
+var intrinioSDK = require('intrinio-sdk');
+const intrinioKey = require('../keys.js');
+intrinioSDK.ApiClient.instance.authentications['ApiKeyAuth'].apiKey = intrinioKey;
+
+
 
 app.use(express.static(path.resolve(__dirname, '..', 'client', 'dist')));
 
-app.get('/test', (req, res) => {
-    res.send('whats up asfasdfasdf');
+app.get('/stocks/:ticker', (req, res) => {
+    const { ticker } = req.params;
+    console.log(ticker);
+    var securityAPI = new intrinioSDK.SecurityApi();
+    
+    var identifier = ticker; // String | A Security identifier (Ticker, FIGI, ISIN, CUSIP, Intrinio ID)
+    
+    var opts = { 
+      'startDate': new Date("2019-01-01"), // Date | Return prices on or after the date
+      'endDate': new Date("2019-01-04"), // Date | Return prices on or before the date
+      'frequency': "daily", // String | Return stock prices in the given frequency
+      'pageSize': 100, // Number | The number of results to return
+      'nextPage': null // String | Gets the next page of data from a previous API call
+    };
+    
+    securityAPI.getSecurityStockPrices(identifier, opts).then(function(data) {
+      res.send(data);
+    }, function(error) {
+      console.error(error);
+      res.send('API call did not work');
+    });
 });
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
